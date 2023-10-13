43c65ba5e6d2e8067e858658d541941e3b7ccbaf
Wesley-Mts <121902297+Wesley-Mts@users.noreply.github.com>
2023-10-13 06:25:35 -0300

    primeiro commit

---
+++ src/main/java/com/spring/SprinTest/SprinTestApplication.java
@@ -0,0 +1,13 @@
+package com.spring.SprinTest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SprinTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SprinTestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/spring/SprinTest/SprinTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spring.SprinTest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SprinTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
